dca05c6806115a9f970f4f0212eed4e8e6de916e
Brendan M <bjohnmurphy1984@gmail.com>
2023-07-12 11:48:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className='d-flex justify-content-center'>
+        <h1 className='tagline'><strong>Weymouth Sons of Italy</strong></h1>
+      </div>
+      <div className='d-flex justify-content-center'>
+        <h3 className='under-tag-line'><i>Lodge No. 278 </i></h3>
+      </div>
+      <div className='d-flex justify-content-center'>
+        <h3 className='under-tag-line'>54 Whitman St, Weymouth, MA 02189</h3>
+      </div>
+    </div>  
   );
 }
 
