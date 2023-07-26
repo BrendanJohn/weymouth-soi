bb0d6638b9c156d6482981aee14002936f791fc4
Brendan M <bjohnmurphy1984@gmail.com>
2023-07-26 15:33:01 -0400

    first big update for soi

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -8,11 +8,6 @@ const Socials = ( ) => {
     </div>
     <div className="col-10 d-flex justify-content-center" id="socials">
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://www.facebook.com/p/Giuseppe-Verdi-Lodge-278-Weymouth-Ma-100068277792803/" />
     </div>
     <div className="col">
     </div>
